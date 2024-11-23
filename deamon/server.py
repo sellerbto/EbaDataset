2ee7d8cb4424e0f3564ae22ccd966fa354d867a7
dimastarsis <kurilin.dmitriy@gmail.com>
2024-11-23 22:27:53 +0500

    пропатчил клиента и сервер

--- deamon/server.py
+++ deamon/server.py
@@ -1,10 +1,11 @@
-from watcher import FileObserver
+from deamon.base import CommandType, ResponseFormatter
+from tracker import DirectoryTrackerManager
 import asyncio
 
 
-async def handle_client(reader, writer, file_observer: FileObserver) -> None:
+async def handle_client(reader, writer, file_observer: DirectoryTrackerManager, shutdown_event: asyncio.Event) -> None:
     addr = writer.get_extra_info("peername")
-    print(f"Клиент подключён: {addr}")
+    print(f"Connected: {addr}")
 
     try:
         while True:
@@ -12,45 +13,51 @@ async def handle_client(reader, writer, file_observer: FileObserver) -> None:
             if not data:
                 break
 
-            command = data.decode().strip().split(" ", 1)
-            action = command[0]
-            argument = command[1] if len(command) > 1 else None
+            request_part = data.decode().strip().split(" ", 1)
+            cmd = CommandType(request_part[0])
+            argument = request_part[1] if len(request_part) > 1 else None
 
-            if action == "add" and argument:
-                response = file_observer.start_watching(argument)
-            elif action == "remove" and argument:
-                response = file_observer.stop_watching(argument)
-            elif action == "list":
-                response = file_observer.list_watched_files()
-            elif action == "exit":
-                response = "Сервер будет остановлен."
-                await writer.drain()
-                asyncio.get_event_loop().stop()
+            result, response = None, None
+            if cmd == CommandType.ADD and argument:
+                result = file_observer.start_watching(argument)
+            elif cmd == CommandType.REMOVE and argument:
+                result = file_observer.stop_watching(argument)
+            elif cmd == CommandType.LIST:
+                result = file_observer.list_watched_files()
+            elif cmd == CommandType.EXIT:
+                response = "Stopping server"
+                shutdown_event.set()
             else:
-                response = "Неизвестная команда."
+                response = f"Wrong command: {cmd.value} {argument}"
+
+            if not response:
+                response = ResponseFormatter.make(cmd, result)
 
             writer.write((response + "\n").encode("utf-8"))
             await writer.drain()
-
     except asyncio.CancelledError:
         pass
     finally:
-        print(f"Клиент отключён: {addr}")
+        print(f"Disconnected: {addr}")
         writer.close()
         await writer.wait_closed()
 
 
-async def main():
-    file_observer = FileObserver()
-    server = await asyncio.start_server(
-        lambda r, w: handle_client(r, w, file_observer), "localhost", 9999
-    )
-    print("Сервер запущен на порту 9999")
+HOST_NAME = "localhost"
+PORT = 9999
 
-    try:
-        async with server:
-            await server.serve_forever()
-    finally:
+
+async def main() -> None:
+    file_observer = DirectoryTrackerManager()
+    shutdown_event = asyncio.Event()
+    server = await asyncio.start_server(
+        lambda r, w: handle_client(r, w, file_observer, shutdown_event), HOST_NAME, PORT
+    )
+    print(f"Work on {HOST_NAME} {PORT} port")
+
+    async with server:
+        await shutdown_event.wait()
+        print("Stopping server")
         file_observer.stop_all()
 
 
